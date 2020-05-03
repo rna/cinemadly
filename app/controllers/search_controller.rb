ec3ca4a465e7f1b511a0ec5e37f087eb7de17e45
Ramesh <rameshnaidu.allu@gmail.com>
2020-05-04 00:57:35 +0530

    render api response on view

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,13 +1,12 @@
 class SearchController < ApplicationController
-  
 
   def search_movie
     @results = OmdbApiClient.new.search_by_title(params[:q])
     if @results["Response"]== "False"
       # session
-      redirect_to users_url, alert:"Movie not found. Please check spelling"
+      redirect_to '/users', alert:"Movie not found. Please check spelling"
     else
-      redirect_to users_url
+      redirect_to users_path(:title => @results['Title'], :poster => @results['Poster'])
     end
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,11 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
-  
+
   def index
+    if params[:title]
+      @title = params[:title]
+      @poster = params[:poster]
+    end
   end
 
   def show
