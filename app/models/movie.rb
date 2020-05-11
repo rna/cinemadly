5c15e0bbca0c0431109e2031eed7afb4772ca361
Ramesh <rameshnaidu.allu@gmail.com>
2020-05-11 16:47:50 +0530

    refactor the movie card view

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -5,4 +5,13 @@ class Movie < ApplicationRecord
   has_many :ratings, dependent: :destroy
 
   scope :ordered_by_most_recent, -> { order(updated_at: :desc) }
+
+  def movie_rating
+    ratings.collect(&:rating)[0]
+  end
+  
+  def average_rating(movie)
+    Rating.where(movie_id:Movie.where(title:movie.title)).average(:rating).to_i
+  end
+
 end
