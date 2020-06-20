4f58f09faf1bdd3917637f9383243b6d23e5d5fd
Ramesh <rameshnaidu.allu@gmail.com>
2020-06-21 01:13:35 +0530

    fix error of spaced string search query with API

--- app/clients/omdb_api_client.rb
+++ app/clients/omdb_api_client.rb
@@ -1,6 +1,7 @@
 class OmdbApiClient
   def search_by_title(title)
-    response = HTTP.get("http://www.omdbapi.com?apikey=#{ENV['OMDB_API_KEY']}&t=#{title}")
+    search_term = URI.encode_www_form_component(title, enc = nil)
+    response = HTTP.get("http://www.omdbapi.com?apikey=#{ENV['OMDB_API_KEY']}&t=#{search_term}")
     JSON.parse(response)
   end
 end
